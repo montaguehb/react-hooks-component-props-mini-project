cb93afaf3a90fbe7b61e76f90e61cc9b0cd8459d
Historia Montague <historia.montague@outlook.com>
2023-05-01 16:49:39 -0600

    added some text after emojis

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const addEmojis = minutes =>  minutes >= 30? '🍱'.repeat(Math.ceil(minutes/10)):'☕️'.repeat(Math.ceil(minutes/5))
-
 const Article = ({post: {title, date, preview, minutes}}) => {
-  const emojis = addEmojis(minutes)
+  const emojis = minutes >= 30? '🍱'.repeat(Math.ceil(minutes/10)):'☕️'.repeat(Math.ceil(minutes/5))
   return (
     <article>
         <h3>{title}</h3>
         <small>{date}.</small>
-        <p>{preview} {emojis}</p>
+        <p>{preview} • {emojis} {minutes} min read</p>
     </article>
   )
 }
