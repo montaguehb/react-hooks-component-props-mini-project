81f148a6cee03e17c96ca366f06320fe1d5e66dd
Historia Montague <historia.montague@outlook.com>
2023-05-01 16:14:37 -0600

    added bonus

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Article = ({post: {title, date, preview}}) => {
+const addEmojis = minutes =>  minutes >= 30? '🍱'.repeat(Math.ceil(minutes/10)):'☕️'.repeat(Math.ceil(minutes/5))
+
+const Article = ({post: {title, date, preview, minutes}}) => {
+  const emojis = addEmojis(minutes)
   return (
     <article>
         <h3>{title}</h3>
-        <small>{date}</small>
-        <p>{preview}</p>
+        <small>{date}.</small>
+        <p>{preview} {emojis}</p>
     </article>
   )
 }
